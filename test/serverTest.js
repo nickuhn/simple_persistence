710182ea038b6d747bb4d636ba0c5d0f0311f12b
Nick Kuhn <nickuhn@gmail.comn>
2015-07-27 20:03:29 -0700

    cleaned up unnecessary describe

--- test/serverTest.js
+++ test/serverTest.js
@@ -6,58 +6,56 @@ var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 describe('server', function() {
-  describe('/get', function() {
-    it('should store a reminder', function(done) {
-      chai.request('localhost:3000')
-        .post('/note')
-        .send({author: 'testing', text: 'tests some more'})
-        .end(function(err, res) {
-          expect(err).to.be.null;
-          expect(res).to.have.status(200);
-          done();
-        });
-    });
-    it('should overwrite a stored reminder', function(done) {
-      chai.request('localhost:3000')
-        .put('/note/note2')
-        .send({author: 'Judge', text: 'Overruled!'})
-        .end(function(err, res) {
-          expect(err).to.be.null;
-          expect(res).to.have.status(200);
-          done();
-        });
-    });
-    it('should delete a stored reminder', function(done) {
-      before(function() {
-        chai.request('localhost:3000').post('/note');
+  it('should store a reminder', function(done) {
+    chai.request('localhost:3000')
+      .post('/note')
+      .send({author: 'testing', text: 'tests some more'})
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
       });
-      chai.request('localhost:3000')
-        .delete('/note/note3')
-        .end(function(err, res) {
-          expect(err).to.be.null;
-          expect(res).to.have.status(200);
-          done();
-        });
+  });
+  it('should overwrite a stored reminder', function(done) {
+    chai.request('localhost:3000')
+      .put('/note/note2')
+      .send({author: 'Judge', text: 'Overruled!'})
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+  it('should delete a stored reminder', function(done) {
+    before(function() {
+      chai.request('localhost:3000').post('/note');
     });
-    it('should retrieve a stored reminder', function(done) {
-      chai.request('localhost:3000')
-        .get('/note/note1')
-        .end(function(err, res) {
-          expect(err).to.be.null;
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          done();
-        });
-    });
-    it('should overwrite a portion of a stored reminder', function(done) {
-      chai.request('localhost:3000')
-        .patch('/note/note2')
-        .send({text: 'Sustained!'})
-        .end(function(err, res) {
-          expect(err).to.be.null;
-          expect(res).to.have.status(200);
-          done();
-        });
-    });
+    chai.request('localhost:3000')
+      .delete('/note/note3')
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+  it('should retrieve a stored reminder', function(done) {
+    chai.request('localhost:3000')
+      .get('/note/note1')
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        done();
+      });
+  });
+  it('should overwrite a portion of a stored reminder', function(done) {
+    chai.request('localhost:3000')
+      .patch('/note/note2')
+      .send({text: 'Sustained!'})
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
   });
 });
